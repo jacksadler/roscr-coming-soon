3aca701df4a7a7414fcf6911a60149ad714ab950
quirkles <al.quirk@gmail.com>
2016-02-13 14:36:55 -0500

    import eslintrc file, update the countdown plugin, begin the restyling there

--- index.js
+++ index.js
@@ -3,23 +3,34 @@ import jQueryMigrate from './jqueryMigrate';
 import bootstrap from 'bootstrap';
 import bootstrapHoverDropDown from './resources/js/bootstrap-hover-dropdown/bootstrap-hover-dropdown.min.js';
 import jQuerySlimscroll from './resources/js/jquery-slimscroll/jquery.slimscroll.min.js';
-import countdown from './resources/js/countdown/jquery.countdown.min.js';
+import countdown from './resources/js/countdown/jquery.countdown.js';
 import backstretch from './resources/js/backstretch/jquery.backstretch.min.js';
 
-jQuery(document).ready(function() {
-  var austDay = new Date();
-  austDay = new Date(austDay.getFullYear() + 1, 1 - 1, 26);
-  $('#defaultCountdown').countdown({until: austDay});
-  $('#year').text(austDay.getFullYear());
+window.jQuery(document).ready(function () {
+  const elemCache = {
+    days: $('#daysCount'),
+    hours: $('#hoursCount'),
+    minutes: $('#minutesCount'),
+    seconds: $('#secondsCount')
+  };
+
+  const finalDate = '2016/03/16 15:30:00';
+  $('#coming-soon-countdown').countdown(finalDate, {
+    precision: 1000
+  }).on('update.countdown', function (event) {
+    elemCache.days.find('.value').text(event.offset.totalDays);
+    elemCache.hours.find('.value').text(event.offset.hours);
+    elemCache.minutes.find('.value').text(event.offset.minutes);
+    elemCache.seconds.find('.value').text(event.offset.seconds);
+  });
   // init background slide images
   $.backstretch([
     './resources/img/backgrounds/3.jpg',
     './resources/img/backgrounds/4.jpg',
     './resources/img/backgrounds/5.jpg',
-    './resources/img/backgrounds/7.jpg',
-    './resources/img/backgrounds/8.jpg'
+    './resources/img/backgrounds/7.jpg'
   ], {
     fade: 1000,
-    duration: 10000
+    duration: 1000
   });
 });
