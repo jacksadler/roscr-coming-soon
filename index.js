6b7e3e7db5f6a7c0ffdad04cd4a58607c0fc9157
quirkles <al.quirk@gmail.com>
2016-02-15 13:45:37 -0500

    add in missing text

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import $ from './initJquery';
 import jQueryMigrate from './jqueryMigrate';
 import bootstrap from 'bootstrap';
-import bootstrapHoverDropDown from './resources/js/bootstrap-hover-dropdown/bootstrap-hover-dropdown.min.js';
+import bootstrapHoverDrown from './resources/js/bootstrap-hover-dropdown/bootstrap-hover-dropdown.min.js';
 import jQuerySlimscroll from './resources/js/jquery-slimscroll/jquery.slimscroll.min.js';
 import countdown from './resources/js/countdown/jquery.countdown.js';
 import backstretch from './resources/js/backstretch/jquery.backstretch.min.js';
@@ -15,6 +15,7 @@ window.jQuery(document).ready(function () {
   };
 
   const finalDate = '2016/03/16 15:30:00';
+
   $('#coming-soon-countdown').countdown(finalDate, {
     precision: 1000
   }).on('update.countdown', function (event) {
@@ -23,6 +24,12 @@ window.jQuery(document).ready(function () {
     elemCache.minutes.find('.value').text(event.offset.minutes);
     elemCache.seconds.find('.value').text(event.offset.seconds);
   });
+
+  $('#subscribe').click(function (e) {
+    e.preventDefault();
+    e.stopPropagation();
+  });
+
   // init background slide images
   $.backstretch([
     './resources/img/backgrounds/3.jpg',
